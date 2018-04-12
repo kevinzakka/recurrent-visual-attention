gradients are all correct now

model.py
@@ -92,16 +92,20 @@ class RecurrentAttention(nn.Module):
         - l_t: a 2D tensor of shape (B, 2). The location vector
           containing the glimpse coordinates [x, y] for the
           current timestep `t`.
-        - b_t: a 2D vector of shape (B, 1). The baseline for the
+        - b_t: a vector of length (B,). The baseline for the
           current time step `t`.
         - log_probas: a 2D tensor of shape (B, num_classes). The
           output log probability vector over the classes.
+        - log_pi: a vector of length (B,).
         """
         g_t = self.sensor(x, l_t_prev)
         h_t = self.rnn(g_t, h_t_prev)
         mu, l_t = self.locator(h_t)
         b_t = self.baseliner(h_t).squeeze()
 
+        # we assume both dimensions are independent
+        # 1. pdf of the joint is the product of the pdfs
+        # 2. log of the product is the sum of the logs
         log_pi = Normal(mu, self.std).log_prob(l_t)
         log_pi = torch.sum(log_pi, dim=1)
 

modules.py
@@ -339,7 +339,7 @@ class location_network(nn.Module):
 
     def forward(self, h_t):
         # compute mean
-        mu = F.tanh(self.fc(h_t))
+        mu = F.tanh(self.fc(h_t.detach()))
 
         # reparametrization trick
         noise = torch.zeros_like(mu)
@@ -377,5 +377,5 @@ class baseline_network(nn.Module):
         self.fc = nn.Linear(input_size, output_size)
 
     def forward(self, h_t):
-        b_t = F.relu(self.fc(h_t))
+        b_t = F.relu(self.fc(h_t.detach()))
         return b_t

trainer.py
@@ -110,12 +110,15 @@ class Trainer(object):
         print('[*] Number of model parameters: {:,}'.format(
             sum([p.data.nelement() for p in self.model.parameters()])))
 
-        # initialize optimizer and scheduler
-        self.optimizer = optim.SGD(
-            self.model.parameters(), lr=self.lr, momentum=self.momentum,
-        )
-        self.scheduler = ReduceLROnPlateau(
-            self.optimizer, 'min', patience=self.lr_patience
+        # # initialize optimizer and scheduler
+        # self.optimizer = optim.SGD(
+        #     self.model.parameters(), lr=self.lr, momentum=self.momentum,
+        # )
+        # self.scheduler = ReduceLROnPlateau(
+        #     self.optimizer, 'min', patience=self.lr_patience
+        # )
+        self.optimizer = optim.Adam(
+            self.model.parameters(), lr=3e-4,
         )
 
     def reset(self):
@@ -126,7 +129,9 @@ class Trainer(object):
         This is called once every time a new minibatch
         `x` is introduced.
         """
-        dtype = torch.cuda.FloatTensor if self.use_gpu else torch.FloatTensor
+        dtype = (
+            torch.cuda.FloatTensor if self.use_gpu else torch.FloatTensor
+        )
 
         h_t = torch.zeros(self.batch_size, self.hidden_size)
         h_t = Variable(h_t).type(dtype)
@@ -454,9 +459,10 @@ class Trainer(object):
             correct += pred.eq(y.data.view_as(pred)).cpu().sum()
 
         perc = (100. * correct) / (self.num_test)
+        error = 100 - perc
         print(
-            '[*] Test Acc: {}/{} ({:.2f}%)'.format(
-                correct, self.num_test, perc)
+            '[*] Test Acc: {}/{} ({:.2f}% - {:.2f}%)'.format(
+                correct, self.num_test, perc, error)
         )
 
     def save_checkpoint(self, state, is_best):
@@ -511,10 +517,10 @@ class Trainer(object):
             print(
                 "[*] Loaded {} checkpoint @ epoch {} "
                 "with best valid acc of {:.3f}".format(
-                    filename, ckpt['epoch']+1, ckpt['best_valid_acc'])
+                    filename, ckpt['epoch'], ckpt['best_valid_acc'])
             )
         else:
             print(
                 "[*] Loaded {} checkpoint @ epoch {}".format(
-                    filename, ckpt['epoch']+1)
+                    filename, ckpt['epoch'])
             )