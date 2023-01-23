549a245d0a54f6cb6a493ddd8877b945d801d8cd
Davide Giacomini <giacomini.davide@outlook.com>
2023-01-23 14:31:12 -0600

    Fixed bugs of hidden_size and input parameters in trainer and model
    The input_size of the CoreNetwork passed through from the model.py was `hidden_size`, which is not actually the real input. The real input is loc_hidden+glimpse_hidden, and I changed it to that.
    Nobody saw it until now because 128+128=256, which is the same size of the hidden_size of h_t.
    
    Moreover, when declaring RecurrentAttention(..), the parameters are swapped. Nobody saw it because glimpse_hidden and locatin_hidden were always the same size (128x128). Although,  if you change size, you can see it.

--- model.py
+++ model.py
@@ -39,7 +39,7 @@ class RecurrentAttention(nn.Module):
         self.std = std
 
         self.sensor = modules.GlimpseNetwork(h_g, h_l, g, k, s, c, quant_bits_g_t, quant_bits_phi, quant_bits_lt)
-        self.rnn = modules.CoreNetwork(hidden_size, hidden_size, quant_bits_h_t)
+        self.rnn = modules.CoreNetwork(h_g + h_l, hidden_size, quant_bits_h_t)
         self.locator = modules.LocationNetwork(hidden_size, 2, std)
         self.classifier = modules.ActionNetwork(hidden_size, num_classes)
         self.baseliner = modules.BaselineNetwork(hidden_size, 1)
