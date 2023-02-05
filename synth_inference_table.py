edd25b4d51cbb7f066def4b8bcebb7fd3a87e324
Davide Giacomini <giacomini.davide@outlook.com>
2023-02-04 19:03:56 -0600

    Fixed bugs in synth_inference_table.py

--- synth_inference_table.py
+++ synth_inference_table.py
@@ -1,5 +1,7 @@
 import csv
+import os
 import random
+import sys
 
 def generate_random_float():
     return random.uniform(0, 1)
@@ -10,7 +12,26 @@ def write_to_file(rows, filename):
         for row in rows:
             writer.writerow(row)
 
-with open('ram_5_4x4_4_3_0_0_2_0.csv') as f:
+if len(sys.argv) != 3:
+    print("Error: Exactly two arguments are required (input and output filenames)")
+    sys.exit(1)
+
+if not all(isinstance(arg, str) for arg in sys.argv[1:]):
+    print("Error: Both arguments must be string paths")
+    sys.exit(1)
+
+input_filename = sys.argv[1]
+output_filename = sys.argv[2]
+
+if not input_filename.endswith(".csv") or not output_filename.endswith(".csv"):
+    print("Error: Both filenames must have the '.csv' file extension")
+    sys.exit(1)
+
+if not os.path.isfile(input_filename):
+    print("Error: Input file does not exist")
+    sys.exit(1)
+
+with open(input_filename) as f:
     reader = csv.reader(f)
     selected_rows = []
     for row in reader:
@@ -18,4 +39,4 @@ with open('ram_5_4x4_4_3_0_0_2_0.csv') as f:
         if random_float <= 0.1:
             selected_rows.append(row)
 
-write_to_file(selected_rows, 'output.csv')
+write_to_file(selected_rows, output_filename)
