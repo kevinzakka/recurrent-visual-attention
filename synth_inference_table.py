67a3160b93182217aba1f9f4c8dfdc6645bdc1a0
Davide Giacomini <giacomini.davide@outlook.com>
2023-02-04 19:21:23 -0600

    Cleaned synth_inference_table.py

--- synth_inference_table.py
+++ synth_inference_table.py
@@ -9,19 +9,17 @@ def generate_random_float():
 def write_to_file(rows, filename):
     with open(filename, 'w', newline='') as f:
         writer = csv.writer(f)
-        for row in rows:
-            writer.writerow(row)
+        writer.writerows(rows)
 
-if len(sys.argv) != 3:
-    print("Error: Exactly two arguments are required (input and output filenames)")
+if len(sys.argv) != 4:
+    print("Error: Exactly three arguments are required (input, output filenames and percentage)")
     sys.exit(1)
 
-if not all(isinstance(arg, str) for arg in sys.argv[1:]):
-    print("Error: Both arguments must be string paths")
-    sys.exit(1)
+input_filename, output_filename, percentage = sys.argv[1:4]
 
-input_filename = sys.argv[1]
-output_filename = sys.argv[2]
+if not all(isinstance(arg, str) for arg in [input_filename, output_filename]):
+    print("Error: Both filenames must be string paths")
+    sys.exit(1)
 
 if not input_filename.endswith(".csv") or not output_filename.endswith(".csv"):
     print("Error: Both filenames must have the '.csv' file extension")
@@ -31,12 +29,21 @@ if not os.path.isfile(input_filename):
     print("Error: Input file does not exist")
     sys.exit(1)
 
+try:
+    copy_percentage = float(percentage)
+except ValueError:
+    print("Error: copy_percentage must be a floating point number")
+    sys.exit(1)
+
+if not 0 <= copy_percentage <= 1:
+    print("Error: copy_percentage must be between 0 and 1")
+    sys.exit(1)
+
 with open(input_filename) as f:
     reader = csv.reader(f)
     selected_rows = []
     for row in reader:
-        random_float = generate_random_float()
-        if random_float <= 0.1:
+        if generate_random_float() <= copy_percentage:
             selected_rows.append(row)
 
 write_to_file(selected_rows, output_filename)
