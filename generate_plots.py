8a150407afe026f2795607b24f0c7552d90def6f
Davide Giacomini <giacomini.davide@outlook.com>
2023-02-17 17:10:16 -0600

    Created code for generating the plot of size_ht

--- generate_plots.py
+++ generate_plots.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import argparse
 
@@ -5,7 +6,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def parse_csv(file_path):
+
+parser = argparse.ArgumentParser(description='Process data from a CSV file')
+parser.add_argument('file', type=str, help='path to the CSV file')
+
+args = parser.parse_args()
+
+# Get the file path from the arguments
+file_path = args.file
+
+font = {'family' : 'serif','size' : 10}
+
+
+def parse_csv():
     # Read the CSV file
     df = pd.read_csv(file_path)
 
@@ -53,31 +66,54 @@ def parse_csv(file_path):
 
 def generate_plot_size_ht(df):
     # filter the rows that you want to compare
-    rows_to_compare = df.iloc[37:54:3]
-    rows_to_compare = rows_to_compare.append(df.loc[1])
-    rows_to_compare = rows_to_compare.sort_values('size_ht')
+    x_quant_0 = df.iloc[36:54:3]
+    x_quant_0 = x_quant_0.append(df.loc[0])
+    x_quant_0 = x_quant_0.sort_values('size_ht')
+
+    x_quant_1 = df.iloc[37:54:3]
+    x_quant_1 = x_quant_1.append(df.loc[1])
+    x_quant_1 = x_quant_1.sort_values('size_ht')
+
+    x_quant_2 = df.iloc[38:54:3]
+    x_quant_2 = x_quant_2.append(df.loc[2])
+    x_quant_2 = x_quant_2.sort_values('size_ht')
 
     # plot the line chart
-    plt.plot(rows_to_compare['size_ht'], rows_to_compare['acc'], c='b', linewidth = 1,label = 'iteration=10', linestyle='-', marker='.')
-    plt.xlabel('Size_ht')
-    plt.ylabel('Accuracy')
+    plt.plot(x_quant_0['size_ht'], x_quant_0['acc'], c='b', label = 'ht not quantized', linewidth = 1, linestyle='-', marker='.')
+    plt.plot(x_quant_1['size_ht'], x_quant_1['acc'], c='r', label = 'ht quantized 1', linewidth = 1, linestyle='-', marker='.')
+    plt.plot(x_quant_2['size_ht'], x_quant_2['acc'], c='g', label = 'ht 1uantized 2', linewidth = 1, linestyle='-', marker='.')
+
+    plt.xlabel('size ht')
+    x_ticks = list(range(16,129,16))
+    plt.xticks(x_ticks)
+    plt.ylabel('Accuracy [%]')
+    plt.ylim(0.65, 1.00)
+    plt.yticks([5*i/100 + 0.70 for i in range(6)])
+    plt.grid(True, linewidth=0.5, color='gray', linestyle=':')
+    plt.legend(fontsize=10)
+
     plt.title('Comparison of accuracy for different size_ht values')
 
-    plt.xticks([16, 32, 48, 64, 80, 96, 128])
+
+    # SAVE GRAPH IN PDF
+    filename = os.path.basename(file_path) # Extract filename
+    root, ext = os.path.splitext(filename)  # split filename and extension
+    parts = root.split('-')    # Split filename into components
+
+    parts[0] = "graphs"    # Modify the desired part
+    dir_name = '-'.join(parts[:])   # Join the parts back into a string with the directory name
+
+    parts[0] = "graph"    # Modify the desired part
+    graph_name = '-'.join(parts[:]) + '.pdf'  # create new filename with new extension
+
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+
+    plt.savefig(os.path.join(dir_name, graph_name), bbox_inches='tight',dpi=300)
 
     return plt
 
-
-
-parser = argparse.ArgumentParser(description='Process data from a CSV file')
-parser.add_argument('file', type=str, help='path to the CSV file')
-
-args = parser.parse_args()
-
-# Get the file path from the arguments
-file_path = args.file
-
-df = parse_csv(file_path)
+df = parse_csv()
 
 plt = generate_plot_size_ht(df)
 
